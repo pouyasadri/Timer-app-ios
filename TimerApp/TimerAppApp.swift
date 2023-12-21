fdf3b5bb1fdbeefdcbd1db3b3a982163fca8afd5
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-21 08:48:05 +0100

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Pouya Sadri on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
